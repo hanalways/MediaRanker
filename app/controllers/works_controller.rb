2bfc36ec1e19f34eebdf1affd16696d5d98bb1dc
Hana <hanalways@gmail.com>
2019-04-23 18:37:05 -0700

    partial view of new work form

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,4 +7,30 @@ class WorksController < ApplicationController
   def show
     @work = Work.find_by(id: params[:id])
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    puts "Creating new media..."
+
+    @work = Work.new(work_params)
+    
+    successful = @work.save
+    if successful
+      flash[:status] = :success
+      flash[:message] = "Successfully saved media with ID #{@work.id}"
+      redirect_to works_path
+    else
+      flash.now[:status] = :error
+      flash.now[:message] = "Could not save media"
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
