8a6c8bb1a0a7b26a7ae3445833dcd7bfdefd8e9c
Hana <hanalways@gmail.com>
2019-04-24 13:37:55 -0700

    implemented view partial for new

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+describe ApplicationHelper do
+  describe "readable_date" do
+    it "produces a tag with the full timestamp" do
+      date = Date.today - 14
+
+      result = readable_date(date)
+
+      expect(result).must_include date.to_s
+    end
+
+    it "returns [unknown] if the date is nil" do
+      date = nil
+
+      result = readable_date(date)
+
+      expect(result).must_equal "[unknown]"
+    end
+  end
+end
