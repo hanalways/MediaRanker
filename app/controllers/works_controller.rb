4bcd5bb26ea0500227a28e184afa64e179c0d72c
Hana <hanalways@gmail.com>
2019-04-23 17:27:18 -0700

    add routes and generate controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,9 +2,12 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
     @categories = ['album', 'book', 'movie']
+    @spotlight = @works.sample
   end
 
   def show
     @work = Work.find_by(id: params[:id])
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "works#index"
+  root "homepage#index"
   resources :works
 end
