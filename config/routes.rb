3fbdd7645975e3bf9840dedcb3211d4cddbc2caa
Hana <hanalways@gmail.com>
2019-04-24 14:27:51 -0700

    updated routes for logins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "homepage#index"
-  resources :works
+  resources :works, :users
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
