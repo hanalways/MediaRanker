ea927e8c9a96f3a3f8667ba02e7f2dfc374e1123
Hana <hanalways@gmail.com>
2019-04-26 07:59:56 -0700

    validation tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,14 +1,42 @@
 require "test_helper"
+require 'pry'
 
 describe Work do
-  let(:work) { Work.new }
+  describe "validations" do 
+    before do 
+      @work = Work.new(
+        title: "test work",
+        creator: "test creator",
+        category: "book",
+        publication_year: 1990,
+        description: "test description"
+      )
+    end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    it "is valid with good data" do 
+      result = @work.valid?
+      
+      expect(result).must_equal true
+    end
+
+    it "is invalid without a title" do 
+      @work.title = nil
+
+      expect(@work.valid?).must_equal false
+    end
+
+    it "is invalid for duplicate creator and title" do 
+      @work.creator = Work.first.creator
+      @work.title = Work.first.title 
+
+      expect(@work.valid?).must_equal false
+    end
   end
 
   describe "entries method" do
     it "returns an array" do
+
     end
   end
+
 end
