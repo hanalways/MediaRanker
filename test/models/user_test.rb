187da703038d08b64784a03585577afdd53244e5
Hana <hanalways@gmail.com>
2019-04-28 16:10:05 -0700

    EXPERIMENTAL: relationships tests for votes and users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,27 +1,41 @@
 require "test_helper"
 
 describe User do
+  before do 
+    @user = User.new(
+      {
+        username: "test user",
+      }
+    )
+  end
+
   describe "validations" do 
-    let(:user) { User.create!(
-      username: "test username"
-      )
-    }
 
     it "is valid when username is present and unique" do 
-      is_valid = user.valid?
+      is_valid = @user.valid?
       expect(is_valid).must_equal true
     end
 
     it "is invalid without a username" do 
-      user.username = nil
-      expect(user.valid?).must_equal false
+      @user.username = nil
+      expect(@user.valid?).must_equal false
     end
 
     it "is invalid if username is non-unique" do 
-      user.username = User.first.username
+      @user.username = User.first.username
 
-      expect(user.valid?).must_equal false
-      expect(user.errors.messages).must_include :username
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :username
     end 
+  end
+
+  describe "relationships" do 
+    it "can add a vote through votes" do 
+      vote = votes.first 
+      @user.votes << vote
+
+      expect(@user.votes).must_include vote
+      expect(vote.user_id).must_equal user.id
+    end
   end
 end
