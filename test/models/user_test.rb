1097b3c85782a89b9e588be9b91b30c59fbcfd3d
Hana <hanalways@gmail.com>
2019-04-28 15:22:34 -0700

    add user validations and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe "validations" do 
+    let(:user) { User.create!(
+      username: "test username"
+      )
+    }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it "is valid when username is present and unique" do 
+      is_valid = user.valid?
+      expect(is_valid).must_equal true
+    end
+
+    it "is invalid without a username" do 
+      user.username = nil
+      expect(user.valid?).must_equal false
+    end
+
+    it "is invalid if username is non-unique" do 
+      user.username = User.first.username
+
+      expect(user.valid?).must_equal false
+      expect(user.errors.messages).must_include :username
+    end 
   end
 end
