0ccd0ed7d7183ff33c954d9cd7a8ab33636de2bd
Hana <hanalways@gmail.com>
2019-04-28 15:31:36 -0700

    add vote validation and tests

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,8 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
+  validates :work_id, uniqueness: { scope: :user_id, :message => "User has already voted for this work" }
+
   def self.get_date(vote)
     return vote.created_at.strftime('%h %d, %Y')
   end
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,35 @@
 require "test_helper"
 
-describe Vote do
-  let(:vote) { Vote.new }
+describe Vote do  
+  describe "validations" do 
+    before do
+      @work = Work.create(
+        title: 'test work',
+        creator: 'test author',
+        category: 'book',
+        publication_year: 1988,
+        description: 'description here'
+      )
+      @user = User.create(
+        username: 'test user'
+      )
+      @vote = Vote.new(
+        user_id: User.find_by(username: 'test user').id,
+        work_id: Work.find_by(title: 'test work').id
+      )
+    end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "is valid when user_id and work_id is a unique combination" do 
+      expect(@vote.valid?).must_equal true
+    end
+
+    it "is invalid when user_id and work_id is a non-unique combination" do 
+      new_vote = Vote.new(
+        user_id: Vote.first.user_id,
+        work_id: Vote.first.work_id,
+      )
+
+      expect(new_vote.valid?).must_equal false 
+    end
   end
 end
