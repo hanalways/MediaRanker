6c51b497bb489f3c0180954d4174afad751e3fe4
Hana <hanalways@gmail.com>
2019-04-28 16:23:11 -0700

    add index, show, login_form, login, logout and current actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,52 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all 
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    unless @user 
+      head :not_found
+    end
+  end
+  
   def login_form
     @user = User.new
+  end
+
+  def login 
+    username = params[:user][:username]
+
+    user = User.find_by(username: username)
+    unless user
+      user = User.create(username: username)
+    end
+
+    session[:user_id] = user.id 
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged in as user #{user.username}"
+
+    redirect_to root_path
+  end
+
+  def logout 
+    session[:user_id] = nil
+
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
+
+    redirect_to root_path
+  end
+
+  def current 
+    @user = User.find_by(id: session[:user_id])
+
+    unless @user 
+      flash[:status] = :error
+      flash[:message] = "You must be logged in to see this page"
+      redirect_to login_path 
+      return
+    end
   end
 end
