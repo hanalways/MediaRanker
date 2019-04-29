fed25ba2446c46d9a0df08d0ff0b9961e15f5a6c
Hana <hanalways@gmail.com>
2019-04-29 04:31:31 -0700

    user summary show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :votes
 
   validates :username, presence: true, uniqueness: true
+
+  def self.get_date(work)
+    return work.created_at.strftime('%h %d, %Y')
+  end
 end
